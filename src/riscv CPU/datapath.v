707e0376b709861437b3933910c1246d7a63e8ab
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2020-05-31 23:10:10 +0200

    new brnach to merge later

--- src/riscv CPU/datapath.v
+++ src/riscv CPU/datapath.v
@@ -23,13 +23,13 @@ module datapath (input clk, input rst);
 
 	wire [31:0]rs1;
 	wire [31:0]rs2;
-	reg_file reg_file0 (.clk(clk), .rst(rst), .src1(inst[`IR_rs1]), .src2(inst[5] ? rs2 : inst[`IR_rs2]), .dest(`IR_rd), .reg_write(reg_write), .write_data(mem_to_reg ? data_mem_out : alu_out), .read1(rs1), .read2(rs2));
+	reg_file reg_file0 (.clk(clk), .rst(rst), .src1(inst[`IR_rs1]), .src2(inst[5] ? rs2 : inst[`IR_rs2]), .dest(inst[`IR_rd]), .reg_write(reg_write), .write_data(mem_to_reg ? data_mem_out : alu_out), .read1(rs1), .read2(rs2));
 
 	wire overflow, is_zero, is_neg, vf;
 	wire [31:0]alu_out;
 	wire beq,bne,bge,blt,bltu,bgeu;
 	alu alu0 (.a(rs1), .b(alu_src ? imm_out : rs2), .shamt(inst[`IR_shamt]), .out(alu_out), .cf(overflow), .zf(is_zero), .vf(vf), .sf(is_neg), .alufn(alu_op));
-	branch brnch1(.rs1(rs1), .rs2(rs2), .imm_out(imm_out), .f3(inst[`IR_funct3]), .should_branch(should_branch));
+	branch branch1(.rs1(rs1), .rs2(rs2), .imm_out(imm_out), .f3(inst[`IR_funct3]), .should_branch(should_branch));
 	wire [31:0]data_mem_out;
 	data_mem data_mem0 (.clk(clk), .addr(alu_out), .write_data(rs2), .mem_write(mem_write), .mem_read(mem_read), .func3(inst[`IR_funct3]), .data_out(data_mem_out));
 
